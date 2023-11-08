df6eb5b410a1cf683eb0f518cfc38ab88b83931b
teosevenses <141769461+teosevenses@users.noreply.github.com>
2023-11-08 13:47:32 +0500

    Update station.rb

--- station.rb
+++ station.rb
@@ -1,24 +1,21 @@
 
 
 class Station
-  attr_accessor :trains, :trains
+  attr_accessor :trains, :name
 
   def initialize(name)
    @name = name
    @trains = []
   end
 
-#вводим в пустой массив приезжающий поезд
   def add_train(train)
   @trains << train
   end
 
-#удаляем из массива поезд который уехал
   def delete_train(train)
   @trains.delete(train)
   end
 
-#делим на грузовые и пассажирские
 
   def train_type(type)
     @trains.select {|train| train.type == type}.count
