9bf2c354fc92830c169f03000cc3f6c34b1d4b0d
teosevenses <141769461+teosevenses@users.noreply.github.com>
2023-11-20 23:37:46 +0500

    Update train.rb

--- train.rb
+++ train.rb
@@ -1,59 +1,20 @@
 class Train
-  attr_reader :wagons, :type, :speed, :num
+  attr_reader :type, :num
+  attr_accessor :route, :station
 
   def initialize(num)
     @num = num
-    @wagons = []
-    @speed = 0
   end
 
-  def add_speed
-    @speed += 1
-  end
-
-  def return_speed
-    @speed -= 1
-  end
-
-  def stop_speed
-    @speed = 0
-  end
-
-  def take_route(route)
-    @route = route
-    @current_station = @route.stations.first
-  end
-
- 
-  def go_next_station
-    @current_station = @route.stations[@route.stations.index(@current_station) + 1]
-  end
-
-  def go_previous_station
-    @current_station = @route.station[@route.stations.index(@current_station) - 1]
-  end
-
-
-  def current_station
-    return unless @route
-    @current_station
-  end
-
-  def next_station
-    return unless @route
-    @route.stations[@route.stations.index(@current_station) +1]
-  end
-
-  def add_wagon(train)
-    if speed == 0 && type == wagon.type
-      @wagons << wagon
+  def move_forward
+    if route != nil && station != nil 
+      self.station = station + 1    
     end
-  end
+  end 
 
-  def del_wagon(wagon)
-    if speed == 0 && type == wagon.type
-      @wagons.delete(wagon)
+  def move_back
+    if route != nil && station != nil 
+      self.station = station - 1    
     end
-  end
-
+  end 
 end
