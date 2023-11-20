16564e331e4ec08b71fa671bd1b71b0f4c37f45f
teosevenses <141769461+teosevenses@users.noreply.github.com>
2023-11-20 23:36:59 +0500

    Update route.rb

--- route.rb
+++ route.rb
@@ -1,11 +1,12 @@
 
 class Route
-  attr_reader :stations
+  attr_reader :stations, :trains
 
   #создаем маршрут
 
   def initialize(first_station, last_station)
     @stations = [first_station, last_station]
+    @trains = []
   end
 
   #добавляем станцию
@@ -18,8 +19,14 @@ class Route
     @stations.delete(station)
   end
 
+  def list_stations
+    puts "Список станций"
+    stations.each_with_index do |x, y| 
+      puts "#{y}. #{x.name}"
+    end
+  end
+
   def readable_stations
       stations.map { |x| x.name }.join("-")
   end
-
 end
